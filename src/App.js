de20b9b0f627d8dfd093e576da011658fe10f4d1
Robert <Robert.Elias@live.com>
2019-12-20 09:10:34 -0800

    imported Welcome.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
 import Header from "./components/Header.js";
-
+import React from "react";
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
